03b4d74ff530ad1d8702bf0bba757f478379a6a0
Ulvy Romy <ulvyromy156@gmail.com>
2025-12-06 16:59:22 +0700

    validate

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+// create-user.dto.ts
+
+import { Prisma } from 'generated/prisma/client';
+
+export class CreateUserDto implements Prisma.UserCreateInput {
+  email: string;
+  name?: string;
+  password: string;
+  // no id, no createdAt, no relations unless you really need them
+}
